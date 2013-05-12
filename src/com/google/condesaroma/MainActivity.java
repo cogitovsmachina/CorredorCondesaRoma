7f2faebe5525804e645d752435783d68029a3ab3
Leonel Mendez <leomindezji@gmail.com>
2013-05-12 02:32:01 -0500

    Using sherlock library and added item contact

--- src/com/google/condesaroma/MainActivity.java
+++ src/com/google/condesaroma/MainActivity.java
@@ -1,10 +1,15 @@
 package com.google.condesaroma;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class MainActivity extends Activity {
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
+
+public class MainActivity extends SherlockActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +19,46 @@ public class MainActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		MenuInflater inflater = getSupportMenuInflater();
+		inflater.inflate(R.menu.main, menu);
 		return true;
 	}
 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.information_contact:
+			informationDialog(MainActivity.this);
+			return true;
+
+		default:
+			return false;
+		}
+
+	}
+
+	private void informationDialog(Context context) {
+
+		AlertDialog.Builder builder;
+		AlertDialog alertDialog;
+		// LayoutInflater inflater = (LayoutInflater) context
+		// .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+		// View layout = inflater.inflate(R.layout.custom_dialog,
+		// (ViewGroup) findViewById(R.id.custom_dialog));
+
+		builder = new AlertDialog.Builder(context);
+		// builder.setView(layout);
+
+		builder.setTitle(context.getResources()
+				.getString(R.string.title_dialog));
+		builder.setMessage(context.getResources().getString(
+				R.string.message_dialog)
+				+ "\n\n" + context.getResources().getString(R.string.link_page));
+
+		alertDialog = builder.create();
+
+		alertDialog.show();
+
+	}
 }
